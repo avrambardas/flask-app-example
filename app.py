ddf0c6801c9002eba39da2ff74ca7dfc4c150849
avrambardas <37124555+avrambardas@users.noreply.github.com>
2024-01-08 15:00:47 +0000

    model refactoring and added new predict for dog classification

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl", "rb"))
-clf = pickle.load(open("models/clf.pkl", "rb"))
+cv = pickle.load(open("models/EmailClassifier/cv.pkl", "rb"))
+clf = pickle.load(open("models/EmailClassifier/clf.pkl", "rb"))
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
+# get the prediction of the email
+@app.route("/predictEmail", methods=["POST"])
+def predictEmail():
     email_text = request.form.get('content')
     tokenized_email = cv.transform([email_text])
     prediction = clf.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template("index.html", prediction=prediction, email_text=email_text)
+
+# get the prediction of thedog image
+@app.route("/predictDog", methods=["POST"])
+def predictDog():
+    email_text = request.form.get('content')
+    tokenized_email = cv.transform([email_text])
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, email_text=email_text)
+
 
 
 if __name__ == '__main__':
